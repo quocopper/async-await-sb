cfa93ecd40ff4c899ac990aa3aa89c9765e04f91
QPham <QPham@copperleaf.com>
2017-04-24 13:28:54 -0700

    Remove commented/obsolete code. Set correct readableObjectMode value.

--- app/binary-upload-test.js
+++ app/binary-upload-test.js
@@ -3,7 +3,7 @@ const url = require( 'url' );
 // const fs = require( 'fs' );
 const uploads_url = 'http://quoc-virtualbox:3002/uploads?onUploaded=http%3A%2F%2Fquoc-virtualbox%3A3001%2Fimporters%2Fasset-types%2F%7Bcontainer%7D';
 const uploads_url_second = 'http://quoc-virtualbox:3002/uploads/%s/upload?onUploaded=http%3A%2F%2Fquoc-virtualbox%3A3001%2Fimporters%2Fasset-types%2F%7Bcontainer%7D';
-const postParams = require( 'url' ).parse( uploads_url );
+const postParams = url.parse( uploads_url );
 const util = require( 'util' );
 const Transform = require( 'stream' ).Transform;
 
@@ -12,42 +12,9 @@ let container_id;
 let newParams;
 
 postParams.method = 'post';
-postParams.someArbitraryField = 'someArbitraryValue';
 
 /**
- *Initial request to create container ID.
- */
-http.request( 
-  postParams, 
-( res )=>{
-
-  // console.log( 'Upload successful! Server responded with:', res.statusCode );
-  // console.log( 'full response:', res.toString() );
-  let bodyContent = '';
-
-  res.on( 'data', ( chunk )=>{
-    
-    bodyContent += chunk;
-
-  } );
-
-  res.on( 'end', ()=>{
-
-    container_id = JSON.parse( bodyContent ).container;
-    console.log( 'The newly created container is:', container_id );
-
-    container_url = util.format( uploads_url_second, container_id );
-    // console.log( 'The container url is:', container_url );
-
-    newParams = url.parse( container_url );
-    newParams.method = 'post';
-
-  } );
-
-} ).end();
-
-/**
- * Use piping here:
+ * POST to /uploads and store the created container ID.
  */
 http.request( 
   postParams, 
@@ -57,6 +24,8 @@ http.request(
     .on( 'data', ( body )=>{
       
       container_id = body.container;
+      container_url = util.format( uploads_url_second, container_id );
+      console.log( 'Container ID:', container_id, '\nContainer URL:', container_url );
     
     } );
 
@@ -66,7 +35,7 @@ function parseBody(){
 
   const accumulator = [];
   const myTransform = Transform( {
-    transform, flush
+    transform, flush, readableObjectMode: true
   } );
 
   function transform( chunk, encoding, myCallback ){
@@ -82,11 +51,12 @@ function parseBody(){
       return myCallback( err );
 
     }
+
   }
 
   function flush( done ){
 
-    const bodyStr = Buffer.from( accumulator ).toString();
+    const bodyStr = Buffer.concat( accumulator ).toString();
     let parsed;
 
     try{
