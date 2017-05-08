a817b117421560d0fba3bf895136cab8b8e0c968
humanSwissArmyKnife <quopperleaf@mail.com>
2017-05-07 18:36:32 -0700

    Modularize body parser

---
+++ app/json-response-body-parser.js
@@ -0,0 +1,49 @@
+'use strict';
+
+
+function createJSONParsingStream(){
+
+  const chunkAccumulator = [];
+
+  function transform( chunk, encoding, next ){
+    
+    try{
+      
+      chunkAccumulator.push( chunk );
+      process.nextTick( next );
+
+    }catch( err ){
+
+      return next( err );
+
+    }
+
+  }
+
+  function flush( done ){
+
+    try {
+      
+      const responseBody = Buffer.concat( chunkAccumulator ).toString();
+      const jsonBody = JSON.parse( responseBody );
+      
+      this.push( jsonBody );
+      process.nextTick( done );
+
+    } catch( err ){
+      
+      return done( err );
+
+    }
+
+  }
+
+  return require( 'stream' ).Transform( {
+    objectMode: true,
+    transform,
+    flush
+  } );
+
+}
+
+module.exports = createJSONParsingStream;
