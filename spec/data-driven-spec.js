fa9bb06b49191971ee7c5d2a1719afe2a71778a1
QPham <QPham@copperleaf.com>
2017-05-24 10:58:47 -0700

    Test Jasmine data provider

---
+++ spec/data-driven-spec.js
@@ -0,0 +1,21 @@
+const using = require( 'jasmine-data-provider' );
+const http = require( 'http' );
+
+const testMatrix = {
+  'This test should pass': { url: 'http://www.google.ca', expected: 200 },
+  'Second test should fail': { url: 'https://www.ggggggooooggggle.abc', expected: 200 }
+};
+
+describe('Query various urls', ()=>{
+
+  using( testMatrix, ( data, description )=>{
+    it( description, ( done )=>{
+      
+      http.request( testMatrix.url, ( res )=>{
+        expect( res.statusCode ).toEqual( data.expected );
+        done();
+      } );
+      
+    });
+  });
+});
